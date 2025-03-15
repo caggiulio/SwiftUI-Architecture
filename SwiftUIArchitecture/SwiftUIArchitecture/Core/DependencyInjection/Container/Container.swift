28c55bae2f54bbf1938658141fa8795c897d8e1e
caggiulio <nunziogiuliocaggegi@gmail.com>
2025-03-15 17:13:30 +0100

    minors

--- SwiftUIArchitecture/SwiftUIArchitecture/Core/DependencyInjection/Container/Container.swift
+++ SwiftUIArchitecture/SwiftUIArchitecture/Core/DependencyInjection/Container/Container.swift
@@ -9,35 +9,31 @@ import Foundation
 extension Container {
   var pokemonRepository: Factory<PokemonRepositoryProtocol> {
     self { Repository.Pokemon() }
-    .scope(.cached)
+      .scope(.cached)
   }
   
   var stateContainer: Factory<StateContainer> {
     self { StateContainer() }
-    .scope(.cached)
+      .scope(.cached)
   }
   
   var coordinator: Factory<Coordinator> {
     self { Coordinator() }
-    .scope(.cached)
+      .scope(.cached)
   }
   
   var networking: Factory<Networking> {
-    self {
-      Networking()
-    }
-    .scope(.cached)
+    self { Networking() }
+      .scope(.cached)
   }
-
+  
   var networkManager: Factory<NetworkManager> {
-    self {
-      NetworkManager(pokemonService: PokemonService())
-    }
-    .scope(.cached)
+    self { NetworkManager(pokemonService: PokemonService()) }
+      .scope(.cached)
   }
-
+  
   var assembler: Factory<Assembler> {
     self { Assembler() }
-    .scope(.cached)
+      .scope(.cached)
   }
 }
